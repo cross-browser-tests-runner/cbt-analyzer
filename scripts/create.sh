18ba7e3ad99127f1fa81dc52196c343ed6ef124a
Reetesh Ranjan <reeteshranjan@piaxis.tech>
2017-11-03 03:54:59 +0530

    SauceLabs analyzer setup
    - Added SauceLabs browsers
    - A script to easily create the cbtr json files for a given platform
    - Setup .travis.yml to use SauceLabs browsers

---
+++ scripts/create.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+platform=$1
+
+if [ "" == "$platform" ]
+then
+  echo "No platform specified"
+  exit 1
+fi
+
+for yml in `ls conf/yml/$platform/*.yml`
+do
+  stem=`basename $yml | sed 's/\.yml//g'`
+  cp ./etc/$platform.json conf/json/$platform/$stem.json
+  ./node_modules/.bin/cbtr-init -i $yml -o conf/json/$platform/$stem.json -u
+done
