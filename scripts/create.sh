3123982194c535a455de8723b82883dfeadc6aa6
Reetesh Ranjan <reeteshranjan@piaxis.tech>
2017-11-29 13:16:50 +0530

    Restructuring of build post analysis of last one

--- scripts/create.sh
+++ scripts/create.sh
@@ -11,6 +11,7 @@ fi
 for yml in `ls conf/yml/$platform/*.yml`
 do
   stem=`basename $yml | sed 's/\.yml//g'`
-  cp ./etc/$platform.json conf/json/$platform/$stem.json
+  echo "working on $stem..."
+  sed "s/{conf}/$stem/g" ./etc/$platform.json > conf/json/$platform/$stem.json
   ./node_modules/.bin/cbtr-init -i $yml -o conf/json/$platform/$stem.json -u
 done
