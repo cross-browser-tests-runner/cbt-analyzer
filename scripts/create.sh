b8949abe6150b2a3eb661b2a50b97a542281ea26
Reetesh Ranjan <reeteshranjan@piaxis.tech>
2017-11-30 04:05:54 +0530

    Fix in tunnel identifiers

--- scripts/create.sh
+++ scripts/create.sh
@@ -11,7 +11,8 @@ fi
 for yml in `ls conf/yml/$platform/*.yml`
 do
   stem=`basename $yml | sed 's/\.yml//g'`
+  stem_dashed=`echo $stem | sed 's/\./-/g'`
   echo "working on $stem..."
-  sed "s/{conf}/$stem/g" ./etc/$platform.json > conf/json/$platform/$stem.json
+  sed "s/{conf}/$stem_dashed/g" ./etc/$platform.json > conf/json/$platform/$stem.json
   ./node_modules/.bin/cbtr-init -i $yml -o conf/json/$platform/$stem.json -u
 done
